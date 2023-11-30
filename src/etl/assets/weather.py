a9c0b29fef1f6dc8798eec4f2ff5511e22d1a796
robertreaney <robertlreaney@gmail.com>
2023-11-30 01:01:02 +0000

    changed weather demo to use library

--- src/etl/assets/weather.py
+++ src/etl/assets/weather.py
@@ -9,7 +9,7 @@ import logging
 from dagster import asset, MaterializeResult
 import warnings
 
-from home_utils import WeatherClient, GmailClient, EmailData, get_secret
+from home_utils import morning_alert
 
 warnings.filterwarnings("ignore", category=ExperimentalWarning)
 
@@ -18,35 +18,7 @@ warnings.filterwarnings("ignore", category=ExperimentalWarning)
 def daily() -> None:
     logging.info('Starting Weather Forecast Update')
 
-    client = WeatherClient(
-        appid=os.getenv("OPEN_WEATHER_MAP_API_KEY"),
-        lat=os.getenv('LAT'),
-        lon=os.getenv('LON')
-        )
-
-    weather = client.get_current()
-    logging.info(weather)
-
-    forecast = client.get_forecast()
-    logging.info(forecast)
-
-    # send forecast as email to user
-    secret = get_secret('dev/tokens/helpful')
-    for key, value in secret.items():
-        os.environ[key] = value
-
-    logging.info('logging into email to listen for messages')
-    # load gmail client
-    google_client = GmailClient()
-
-    google_client.send_message(
-        to='3863663150@mms.att.net', 
-        subject=None, 
-        message_text=weather + '\n\n' + forecast, 
-        thread_id=None, 
-        in_reply_to=None)
-
-    return 
+    morning_alert()
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
